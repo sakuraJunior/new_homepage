ad7a5045eb03b5ae5bcbbeaeeb2578b43c372b2e
tantan-sakuragumi <58903058+tantan-sakuragumi@users.noreply.github.com>
2020-04-25 14:13:59 +0900

    Create cm2deg.js

---
+++ assets/js/cm2deg.js
@@ -0,0 +1,11 @@
+const cmElm = document.getElementById("cm");
+const sizeElm = document.getElementById("size");
+const degElm = document.getElementById("deg");
+for(const elm of [cmElm, sizeElm]) {
+    elm.addEventListener("input", () => {
+        const cm = cmElm.value;
+        const size = sizeElm.value;
+        const deg = cm / Math.PI / size * 360;
+        degElm.innerHTML = deg;
+    });
+}
