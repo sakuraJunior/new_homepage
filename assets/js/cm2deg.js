fee50dafcc0987ff25b4e8db4f77dff5cc12e971
tantan-sakuragumi <58903058+tantan-sakuragumi@users.noreply.github.com>
2020-04-25 15:23:03 +0900

    Update cm2deg.js

--- assets/js/cm2deg.js
+++ assets/js/cm2deg.js
@@ -6,6 +6,6 @@ for(const elm of [cmElm, sizeElm]) {
         const cm = cmElm.value;
         const size = sizeElm.value;
         const deg = cm / Math.PI / size * 360;
-        degElm.innerHTML = Number.isFinite(deg) ? deg : "---";
+        degElm.innerHTML = Number.isFinite(deg) ? Math.round(deg*1000)/1000 : "---";
     });
 }
